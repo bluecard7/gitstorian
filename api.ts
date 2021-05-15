de67c9573d05175d4986c499582c36b949ad444c
Paul Yang <paul.junho.yang@gmail.com>
2021-05-14 22:37:01 -0700

    init

---
+++ api.ts
@@ -0,0 +1,1 @@
+// file for code that calls git in a subprocess and parse it's output
---
+++ main.ts
@@ -0,0 +1,28 @@
+// permissions
+const { request: permRequest } = Deno.permissions;
+const runPerm = await permRequest({ name: "run" })
+if (runPerm.state !== 'granted') {
+  console.log(`'run' permission denied`)
+  Deno.exit(1)
+} 
+const readPerm = await permRequest({ name: "read" })
+if (readPerm.state !== 'granted') {
+  console.log(`'read' permission denied`)
+  Deno.exit(1)
+} 
+
+const gitRepo = '../deno'
+
+Deno.chdir(gitRepo)
+const p = Deno.run({ 
+  cmd: ['sh'],
+  stdin: 'piped',
+  stdout: 'piped',
+})
+
+const cmd = 'git log --reverse | head -n5'
+await p.stdin.write(new TextEncoder().encode(cmd))
+await p.stdin.close()
+const out = await p.output()
+console.log(new TextDecoder().decode(out))
+p.close()
