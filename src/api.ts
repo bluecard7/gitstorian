b6df33c168d04c44dc1dd65b3929094e403900c4
Paul Yang <paul.junho.yang@gmail.com>
2021-05-28 11:11:25 -0700

    continued reorg

--- src/api.ts
+++ src/api.ts
@@ -3,84 +3,11 @@
 import { existsSync } from "https://deno.land/std/fs/mod.ts";
 import { execCtx } from "./ctx.ts";
 
-
 // TODO: gitstorian command for viewing diffs
 // just copy to Deno.stdout? Does less work in that case?
 
 const encoder = new TextEncoder();
 const decoder = new TextDecoder();
-
-class GitCommandFormatter {
-  hashes(fromCommit: string): string {
-    const from = fromCommit ? `${fromCommit}..` : "HEAD";
-    return `git rev-list --reverse ${from} | head -n5`;
-  }
-
-  // instead having this process the filename, do that cache?
-  diff(commit: string, filename: string): string {
-    let gitCmd = "show";
-    const defaults = `--oneline ${filename ? "" : "--stat"}`;
-    const fileOpt = filename ? `-- ${filename}` : "";
-    return `git ${gitCmd} ${defaults} ${commit} ${fileOpt}`;
-  }
-}
-
-// todo: change name
-// not really a cache, but a commit... manager?
-// just happens to cache as well
-// Or maybe there should be another class to describe purely handling commits
-class CommitCache {
-  pos: number;
-  cache: string[];
-  // file to persist session
-  storeName: string;
-
-  constructor () {
-    this.pos = 0;
-    this.cache = [];
-    this.storeName = ".ripthebuild";
-    // in case persisted data exists
-    this._loadPersisted();
-    // if there was none (or it was empty), run git command
-    this.cache.length === 0 && this.hydrate();
-  }
-
-  _loadPersisted() {
-    const { cache, storeName } = this
-    if (existsSync(storeName)) {
-      const blob = decoder.decode(Deno.readFileSync(storeName));
-      cache = blob.split("\n").filter(Boolean);
-    }
-  }
-
-  current(): string {
-    const { pos, cache } = this;
-    return pos < cache.length ? cache[pos] : "";
-  }
-
-  async hydrate(gitFmtr) {
-    const cmd = fmtHashCmd(this.current());
-    state.cache = (await run(cmd)).split("\n").filter(Boolean);
-  }
-
-  async next() {
-    const { pos, cache } = this;
-    if (pos < cache.length) {
-      this.pos += 1;
-      return;
-    }
-    await this.hydrate();
-  }
-
-  persist() {
-    const { pos, cache, storeName } = this;
-    const data = cache.slice(pos).join("\n");
-    console.log(`[PERSISTING]\n${data}`);
-    Deno.writeFileSync(storeName, encoder.encode(data), { create: true });
-  }
-}
-
-const commitCache = new CommitCache();
 
 // <command> [filename?]
 interface Options {
@@ -97,7 +24,6 @@ async function run(cmd: string): Promise<string> {
   if (execCtx.is(ExecCtx.Test)) {
     return "not spawning process in test";
   }
-
   console.log("[RUNNING]:", cmd);
   const p = Deno.run({
     cmd: ["sh"],
@@ -110,6 +36,71 @@ async function run(cmd: string): Promise<string> {
   p.close();
   return decoder.decode(out);
 }
+
+function fmtHashesCmd(fromCommit: string): string {
+  const from = fromCommit ? `${fromCommit}..` : "HEAD";
+  return `git rev-list --reverse ${from} | head -n5`;
+}
+
+// instead having this process the filename, do that cache?
+function fmtDiffCmd(commit: string, filename: string): string {
+  let gitCmd = "show";
+  const defaults = `--oneline ${filename ? "" : "--stat"}`;
+  const fileOpt = filename ? `-- ${filename}` : "";
+  return `git ${gitCmd} ${defaults} ${commit} ${fileOpt}`;
+}
+
+// todo: change name
+// not really a cache, but a commit... manager?
+// just happens to cache as well
+// Or maybe there should be another class to describe purely handling commits
+class CommitCache {
+  pos: number;
+  cache: string[];
+  // file to persist session
+  storeName: string;
+
+  constructor() {
+    this.pos = 0;
+    this.cache = [];
+    this.storeName = ".ripthebuild";
+    // in case persisted data exists
+    this._loadPersisted();
+  }
+
+  _loadPersisted() {
+    if (existsSync(this.storeName)) {
+      const blob = decoder.decode(Deno.readFileSync(this.storeName));
+      this.cache = blob.split("\n").filter(Boolean);
+    }
+  }
+
+  back = () => this.pos -= 1;
+  _current = (): string => this.cache[this.pos] || "";
+  forward = () => this.pos += 1;
+  async read(): string {
+    let value = this._current();
+    if (this.pos + 1 === this.cache.length) {
+      await this._hydrate();
+      this.pos = 0;
+    }
+    return value;
+  }
+
+  async _hydrate() {
+    const cmd = fmtHashesCmd(this._current());
+    this.cache = (await run(cmd)).split("\n").filter(Boolean);
+  }
+
+  persist() {
+    const { pos, cache, storeName } = this;
+    const data = cache.slice(pos).join("\n");
+    console.log(`[PERSISTING]\n${data}`);
+    Deno.writeFileSync(storeName, encoder.encode(data), { create: true });
+  }
+}
+
+const commitCache = new CommitCache();
 
 export async function setup(repoPath: string): Promise<{
   success: boolean;
@@ -133,23 +124,17 @@ export async function setup(repoPath: string): Promise<{
   return { success, errMsg };
 }
 
-async function interpret(line: string) {
+export async function request(line: string): Promise<string> {
   const { cmd, filename } = parse(line);
-  cmd[0] === "n" && await cache.next();
+  cmd[0] === "n" && commitCache.next();
   if (cmd[0] === "v") {
     // do nothing, just show current commit
   }
-  
+  const commit = await commitCache.read()
+  return run(fmtDiffCmd(commit, filename));
 }
 
-export async function request(line: string): Promise<string> {
-  const opts = parse(line);
-  const gitCmd = await buildGitCmd(opts, state);
-  return run(gitCmd);
-}
-
-export function persist() { cache.persist(); }
-
+export const { persist } = commitCache;
 export const testExports = {
   parse,
-}
+};
