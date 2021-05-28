f0543cea0f8b13d119cbc20fc409fae587adc891
Paul Yang <paul.junho.yang@gmail.com>
2021-05-27 23:07:34 -0700

    matcher function for url and method

--- server.ts
+++ server.ts
@@ -1,34 +1,43 @@
 import * as http from "https://deno.land/std@0.97.0/http/server.ts";
-import * as ripthebuild from './api.ts'; // maybe rename to lib?
+import * as ripthebuild from "./api.ts"; // maybe rename to lib?
 // deno run --allow-net --allow-run --allow-read --allow-write server.ts
 
 const server = http.serve({ port: 8081 });
 console.log(`HTTP webserver running.  Access it at:  http://localhost:8081/`);
 
+function matchURLAndMethod(
+  { url, method }: http.ServerRequest,
+  expectedMethod: string,
+  expectedURL: string,
+): boolean {
+  // todo: support for query params
+  return url === expectedURL && method === expectedMethod;
+}
+
 async function handle(req: http.ServerRequest): Promise<http.Response> {
-  const { url, method } = req;
-  if (url === '/setup' && method === 'POST') {
-    const repoPath = new TextDecoder().decode(await Deno.readAll(req.body))
-    const { success, errMsg } = await ripthebuild.setup(repoPath)
-    return { 
-      status: success ? 200 : 409, 
-      body: success ? 'setup complete' : errMsg,
-    }
+  if (matchURLAndMethod(req, "POST", "/repo/mv")) {
+    const repoPath = new TextDecoder().decode(await Deno.readAll(req.body));
+    const { success, errMsg } = await ripthebuild.setup(repoPath);
+    return {
+      status: success ? 200 : 409,
+      body: success ? "setup complete" : errMsg,
+    };
   }
+  if (matchURLAndMethod(req, "GET", "/prev")) {
+    return { status: 200, body: await ripthebuild.request("p") };
+  }
+  // behavior if repo isn't setup is to use cwd...
+  // also need to handle when git commands make no sense -> cwd is not a git repo
   // maybe better path is /commit/...
-  if (url === '/curr' && method === 'GET') {
+  if (matchURLAndMethod(req, "GET", "/curr")) {
     // if ?file=<> is provided, then line should be `v ${file}`
     // similarly for the other paths
-    return { status: 200, body: await ripthebuild.request('v') }
+    return { status: 200, body: await ripthebuild.request("v") };
   }
-  if (url === '/next' && method === 'GET') {
-    return { status: 200, body: await ripthebuild.request('n') }
+  if (matchURLAndMethod(req, "GET", "/next")) {
+    return { status: 200, body: await ripthebuild.request("n") };
   }
-  if (url === '/prev' && method === 'GET') {
-    return { status: 200, body: await ripthebuild.request('p') }
-  }
-  if (
-  return { status: 404, body: 'unknown path' }
+  return { status: 404, body: "unknown path" };
 }
 
 for await (const request of server) {
