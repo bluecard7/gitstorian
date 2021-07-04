05436a39874d6a5bb187af411393bc53cb5782dd
Paul Yang <paul.junho.yang@gmail.com>
2021-07-03 17:13:57 -0700

    frontend draft

--- next-ui/pages/index.tsx
+++ next-ui/pages/index.tsx
@@ -2,80 +2,111 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Fragment, useCallback, useEffect, useState } from 'react'
 import { useSpring, animated } from 'react-spring'
+import { fromEvent } from 'rxjs'
+import { filter, map, throttleTime } from 'rxjs/operators'
 import styles from '../styles/Home.module.css'
 
-function useAPI() {
-  const [hashes, setHashes] = useState([])
-  const [diff, setDiff] = useState("")
-  const baseURL = 'http://localhost:8081'
-
-  async function loadHashes(order: string, hash: string): Promise<string[]> {
-    const data = await (
-      fetch(`${baseURL}/commits/${order}/${hash}`)
-        .then(data => data)
-        .catch(err => ({ text: () => err.message }))
-    )
-    const page = data.ok ? (await data.json()) : []
-    if (page.length) {
-      setHashes(page)
-      return page
-    }
-    return hashes;
-  }
-
-  // if TypeError, abort all requests after?
-  async function loadDiff(hash: string, filename: string): Promise<string> {
-    // if caching, return if already requested
-    const data = await (
-        fetch(`${baseURL}/diffs/${hash}/${filename}`)
-        .then(data => data)
-        .catch(err => ({ text: () => err.message }))
-    )
-    // await data.json()?
-    const text = (await data.text()).trim();
-    // if (data.ok) { 
-    //  todo: cache texts + avoid requests
-    // }
-    setDiff(text)
-    return text
-  }
-  return { 
-    data: { hashes, diff },
-    load: { 
-      hashes: loadHashes, 
-      diff: loadDiff, 
-    }
-  };
-}
-
-function FrameMenu({ hash, diff, load }) {
-  const formatMenu = () => {
-    return diff.split('\n')
-      .map(line => line.split('|'))
-      .map(parts => parts.length === 2 && parts[0].trim())
-      .map(filename => filename && (
-        <button onClick={() => load.diff(hash, filename)}>
-          {filename}
-        </button>
-      ))
-  }
-  const [menu, setMenu] = useState(formatMenu())
-
-  useEffect(() => { setMenu(formatMenu) }, [hash])
-
-  return (
-    <Fragment>
-      {menu}
-    </Fragment>
+const baseURL = 'http://localhost:8081'
+const keydownObserver = fromEvent(document, 'keydown')
+  .pipe(
+    throttleTime(500),
+    map(e => e.code),
+    filter(Boolean),
   )
+
+const urlify = (parts: string[]): string => parts.filter(Boolean).join('/')
+function fetchData(pieces: string[]): Promise<Response> {
+  return fetch(urlify(pieces))
+    .then(data => data)
+    .catch(err => ({ text: () => err.message }))
 }
 
-const PAGE_SIZE = 10
-function Frame() {
-  const { data, load } = useAPI()
-  // as in pos in data.hashes
-  const [pos, setPos] = useState(PAGE_SIZE)
+async function loadPage(
+  order: string = "", 
+  hash: string = "", 
+  path: string = "",
+): Promise<string[]> {
+  const res = await fetchData([baseURL, 'commits', order, hash, path])
+  return res.ok ? (await res.json()) : []
+}
 
+async function loadDiff(
+  hash: string = "", 
+  path: string = ""
+): Promise<{ diff: string, menu: string[]}> {
+  const res = await fetchData([baseURL,'diffs', hash, path])
+  return data.json()
+}
+
+function useCommits() {
+  const [hashes, setHashes] = useState([])
+  const [pos, setPos] = useState(0)
+  useEffect(() => {
+    const subscription = keydownObserver.subscribe(code => {
+      switch (code) {
+        case 'ArrowLeft':
+          if (pos === 0) {
+            // instead of array, could try traversing a bin tree of hash pages
+            loadPage('prev', hashes[pos]).then(page => {
+              setHashes([...page, ...hashes])
+              setPos(old => old - 1)
+            })
+          }
+          return setPos(old => old - 1)
+        case 'ArrowRight':
+          if (pos + 1 === hashes.length) {
+            loadPage('next', hashes[pos]).then(page => {
+              setHashes([...hashes, ...page])
+              setPos(old => old + 1)
+            })
+          }
+          return setPos(old => old + 1)
+      }
+    });
+    return subscription.unsubscribe
+  }, [])
+
+  // how do I provide a path to iterate on?
+  // how/when to trigger fetch in subscription?
+  return { hash: hashes[pos] }
+}
+
+/*function formatMenu(diff) {
+  return diff.split('\n')
+    .map(line => line.split('|'))
+    .map(parts => parts.length === 2 && parts[0].trim())
+    .filter(Boolean)
+}*/
+
+function useDiff() {
+  const [pathMenu, setMenu] = useState([])
+  const [pos, setPos] = useState(0)
+  const { hash } = useCommits()
+  useEffect(() => {
+    const subscription = keydownObserver.subscribe(code => {
+      switch (code) {
+        case 'ArrowUp':
+          return setMenuPos(pos => pos + 1)
+        case 'ArrowDown':
+          return setMenuPos(pos => pos - 1)
+        case 'Enter':
+        // todo: provide the menu through backend response
+        // w/ the diff
+          return // loadDiff(hash, pathMenu[pos]).then(
+          // res => { setMenu(res.menu); setDiff(res.diff) }
+          // )
+      }
+    });
+    return subscription.unsubscribe
+  }, [])
+
+  // how to show which one is currently chosen?
+  // - text difference? aka add * to current chosen
+  return { diff, pathMenu }
+}
+
+function Frame() {
+  const { diff, pathMenu } = useDiff();
   const fadeStyle = useSpring({
     from: { opacity: 0.3 },
     to: { opacity: 1 },
@@ -88,86 +119,38 @@ function Frame() {
     reset: true,
   })
 
-  useEffect(() => {
-    async function handleKey({ code }) {
-      if (code === 'ArrowLeft') {
-        setPos(prevPos => prevPos - 1)
-      }
-      if (code === 'ArrowRight') {
-        setPos(prevPos => prevPos + 1)
-      }
-    }
-    window?.addEventListener('keydown', handleKey)
-    return() => window?.removeEventListener('keydown', handleKey)
-  }, [])
-
-  useEffect(() => {
-    console.log("[pos]", pos, data.hashes)
-    let pagePromise: Promise<string[]>;
-    if (!data.hashes.length) {
-      pagePromise = load.hashes('next', '')
-    }
-    if (pos < 0) {
-      pagePromise = load.hashes('prev', data.hashes[0])
-      setPos(PAGE_SIZE - 1)
-    }
-    if (pos === PAGE_SIZE) {
-      pagePromise = load.hashes('next', data.hashes[PAGE_SIZE - 1])
-      // caching would remove the second request I think will happen b/c of this line
-      setPos(0)
-    }
-    pagePromise?.then(hashes => load.diff(hashes[pos] || '', ''))
-    !pagePromise && load.diff(data.hashes[pos] || '', '')
-  }, [pos, data.hashes])
-
-  // ------- menu ---------
-  // doesn't update to the current hash for some reason
-  // just calling formatMenu for now
-  const formatMenu = () => {
-    return data.diff.split('\n')
-      .map(line => line.split('|'))
-      .map(parts => parts.length === 2 && parts[0].trim())
-      .map(filename => filename && (
-        <button onClick={() => load.diff(data.hashes[pos], filename)}>
-          {filename}
-        </button>
-      ))
-  }
-  const [menu, setMenu] = useState(formatMenu())
-
-  useEffect(() => { setMenu(formatMenu) }, [pos])
-  // ------- menu ---------
-
-  const lines = data.diff.split('\n')
-  // rows and cols padded to avoid scrolling + wrapping
-  const dims = {
-    rows: lines.length + 1,
-    cols: Math.max(...lines.map(line => line.length)) + 5,
-  }
-
+  const lines = diff.split('\n')
+  const longestLineLen = Math.max(...lines.map(line => line.length))
+  
   return (
     <Fragment>
       <animated.textarea 
         style={fadeStyle} 
-        {...dims}
-        value={data.diff} 
+        // rows and cols padded to avoid scrolling + wrapping
+        rows={lines.length + 1}
+        cols={longestLineLen + 5}
+        value={diff}
         readOnly 
       />
-      {/* Assumes --stat view is shown first */}
-      {menu}
+      {/* still allow a user to click on menu?
+          thinking it would fire a keydown event to trigger above
+          - throttle might be an issue though
+        */
+        menu.map(path => (
+          <button onClick={() => loadDiff(hash, filename)}>
+            {filename}
+          </button>
+        ))}
     </Fragment>
   )
 }
 
-export default function Home() {
+export default () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Generated by create next app" />
-        <link rel="icon" href="/favicon.ico" />
+        <title>ripthebuild</title>
       </Head>
-
       <main className={styles.main}>
         <Frame />
       </main>
