a0866814732b95a8613eb88b16c32bd94b213d82
Paul Yang <paul.junho.yang@gmail.com>
2021-07-04 18:39:53 -0700

    can move back and forth w/ menu, able to traverse menu

--- next-ui/pages/index.tsx
+++ next-ui/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Fragment, useCallback, useEffect, useState } from 'react'
+import { Fragment, useRef, useCallback, useEffect, useState } from 'react'
 import { useSpring, animated } from 'react-spring'
 import { of, fromEvent } from 'rxjs'
 import { filter, map, throttleTime } from 'rxjs/operators'
@@ -9,13 +9,12 @@ import styles from '../styles/Home.module.css'
 const baseURL = 'http://localhost:8081'
 const observeKeydown = () => fromEvent(document, 'keydown')
   .pipe(
-    throttleTime(500),
+    throttleTime(300),
     map(e => e.code),
     filter(Boolean),
   )
 const urlify = (parts: string[]): string => parts.filter(Boolean).join('/')
 const fetchData = (pieces: string[]): Promise<Response> => {
-  console.log(urlify(pieces))
   return fetch(urlify(pieces))
     .then(data => data)
     .catch(err => ({ text: () => err.message }))
@@ -31,80 +30,119 @@ async function loadPage(
   return res.ok ? (await res.json()) : []
 }
 
+// todo: file renames result in both versions being appended w/ =>
+// handle in backend
 async function loadDiff(
   hash: string = "", 
   path: string = ""
-): Promise<{ diff: string[], menu: string[]}> {
+): Promise<{ diff: string[], pathMenu: string[]}> {
   const res = await fetchData([baseURL,'diffs', hash, path])
   return res.json()
 }
 
-function useCommits(keydownObserver) {
-  const [hashes, setHashes] = useState([])
-  const [pos, setPos] = useState(0)
+// being used as base of useDiff, but these were designed to be complimentary
+// separate + use in Frames?
+function useCommits() {
+  const hashes = useRef([])
+  const [pos, setPos] = useState(-1)
+  const posRef = useRef(-1)
   const [path, setPath] = useState("")
+
+  function updatePos(value: number) {
+    posRef.current = value
+    setPos(value)
+  }
+
+  // todo: issue, once reaching end, concats first page to end
+  // similar situation going backwards
+  // may or may not be an issue, could just make hashes a fixed size and move with it
+  // want numbering of commits?
   useEffect(() => {
     // get the first page
-    loadPage("next").then(page => setHashes(page))
-    const subscription = keydownObserver.subscribe(code => {
+    loadPage("next").then(page => {
+      hashes.current = page
+      updatePos(0)
+    })
+    const subscription = observeKeydown().subscribe(code => {
       // todo: need to check if page is empty or not
+      const currPos = posRef.current
       switch (code) {
         case 'ArrowLeft':
-          0 < pos && setPos(old => old - 1)
-          pos === 0 && loadPage('prev', hashes[pos], path).then(page => {
-            setHashes([...page, ...hashes])
-            setPos(page.length - 1)
+          currPos > 0 && updatePos(currPos - 1)
+          currPos === 0 && loadPage('prev', hashes.current[currPos], path).then(page => {
+            hashes.current = page.concat(hashes.current)
+            updatePos(page.length - 1)
           })
           break
         case 'ArrowRight':
-          const last = hashes.length - 1
-          pos < last && setPos(old => old + 1)
-          pos === last && loadPage('next', hashes[pos], path).then(page => {
-            setHashes([...hashes, ...page])
-            setPos(old => old + 1)
+          const last = hashes.current.length - 1
+          currPos < last && updatePos(currPos + 1)
+          currPos === last && loadPage('next', hashes.current[currPos], path).then(page => {
+            hashes.current = hashes.current.concat(page)  
+            updatePos(currPos + 1)
           })
       }
     });
-    return subscription.unsubscribe
+    return subscription?.unsubscribe
   }, [])
-  // hash could be undefined
-  return { hash: hashes[pos], setPath }
+  // console.log(hashes, pos)
+  return { hash: hashes.current[pos], setPath }
 }
 
-function useDiff(keydownObserver) {
+function useDiff() {
+  const menu = useRef([])
   const [diff, setDiff] = useState([])
-  const [pathMenu, setMenu] = useState([])
   const [pos, setPos] = useState(0)
-  const { hash, setPath } = useCommits(keydownObserver)
+  const posRef = useRef(0)
+  // setPath is bad name, more like specifyPage, narrowPage??
+  const { hash, setPath } = useCommits()
+
+  function updatePos(newPos: number) {
+    if (newPos < 0 || newPos === menu.current.length) return 
+    posRef.current = newPos
+    setPos(newPos)
+  }
 
   useEffect(() => {
-    hash && loadDiff(hash, pathMenu[pos]).then(res => {
-        res.menu && setMenu(res.menu)
-        res.diff && setDiff(res.diff) 
+    // todo, instead need Enter key to specify path is to be used
+    // like, a user is "selecting" once they nav through the menu
+    // And once enter is hit, then loadDiff can use it to determine
+    // what kind of diff is generated.
+    // 
+    // for now ignoring path
+    // const path = selecting ? "" : menu.current[pos]
+    hash && loadDiff(hash).then(res => {
+        menu.current = res.pathMenu || []
+        setDiff(res.diff)
+        updatePos(0)
     })
   }, [hash])
 
   useEffect(() => {
-    const subscription = keydownObserver.subscribe(code => {
+    const subscription = observeKeydown().subscribe(code => {
+      const currPos = posRef.current
       switch (code) {
         case 'ArrowUp':
-          return setPos(old => old + 1)
+          return updatePos(currPos - 1)
         case 'ArrowDown':
-          return setPos(old => old - 1)
+          return updatePos(currPos + 1)
         case 'Enter':
-          return setPath(pathMenu[pos] || "")
+          // should set pages to only include this path
+          return setPath(menu.current[currPos] || "")
       }
     });
-    return subscription.unsubscribe
+    return subscription?.unsubscribe
   }, [])
+  console.log(menu.current[pos])
   // how to show which one is currently chosen?
   // - text difference? aka add * to current chosen
-  return { diff, pathMenu }
+  //
+  // goin through menu cause animation to refresh, even though same diff
+  return { diff, menu: menu.current }
 }
 
 function Frame() {
-  const [keydownObserver, setObserver] = useState(of())
-  const { diff, pathMenu } = useDiff(keydownObserver);
+  const { diff, menu } = useDiff();
   const fadeStyle = useSpring({
     from: { opacity: 0.3 },
     to: { opacity: 1 },
@@ -117,9 +155,7 @@ function Frame() {
     reset: true,
   })
 
-  useEffect(() => setObserver(observeKeydown()), [])
-
-  console.log("res", diff, pathMenu)
+  console.log(menu)
   const longestLineLen = Math.max(...diff.map(line => line.length))
   return (
     <Fragment>
@@ -128,17 +164,15 @@ function Frame() {
         // rows and cols padded to avoid scrolling + wrapping
         rows={diff.length + 1}
         cols={longestLineLen + 5}
-        value={diff}
+        value={diff.join("\n")}
         readOnly 
       />
       {/* still allow a user to click on menu?
           thinking it would fire a keydown event to trigger above
           - throttle might be an issue though
         */
-        pathMenu.map(path => (
-          <div>
-            {filename}
-          </div>
+        menu.map(path => (
+          <div>{path}</div>
         ))}
     </Fragment>
   )
